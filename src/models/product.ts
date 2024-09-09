ec13d2ae1e5a4b7cff8e205639d1c4e181d8c3c9
Mandeep Kumar <mk1773612@gmail.com>
2024-09-09 00:57:03 -0300

    Intial commit

---
+++ src/models/product.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { trim } from "validator";
+
+
+
+
+const schema = new mongoose.Schema(
+    {
+        // _id: {
+        //     type: String,
+        //     required:[true,"Please enter ID"],          
+        // },
+        name: {
+            type: String,
+            required:[true,"Please enter Name"],          
+        },
+        photo: {
+            type: String,
+            required:[true,"Please add Photo"],          
+        },
+        price: {
+            type: Number,
+            required:[true,"Please enter Price"],          
+        },
+        stock: {
+            type: Number,
+            required:[true,"Please enter Stock"],          
+        },
+        category: {
+            type: String,
+            required:[true,"Please enter Category"],
+            trim: true,          
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+export const Product = mongoose.model("Product", schema);
---
+++ src/routes/products.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+import { adminOnly } from "../middlewares/auth.js";
+import { deleteProduct, getAdminProducts, getAllCategories, getAllProducts, getLatestProducts, getSingleProduct, newProduct, updateProduct } from "../controllers/product.js";
+import { singleUpload } from "../middlewares/multer.js";
+
+const app = express.Router();
+
+// Create New Product - /api/v1/product/new
+app.post("/new", adminOnly, singleUpload, newProduct);
+
+// Get all products with filters - /api/v1/product/all
+app.get("/all", getAllProducts);
+
+// Get last 5 Products - /api/v1/product/latest
+app.get("/latest", getLatestProducts);
+
+// Get all unique Categories - /api/v1/product/categories
+app.get("/categories", getAllCategories);
+
+//Get all Products - /api/v1/product/admin-products
+app.get("/admin-products", adminOnly, getAdminProducts );
+
+// Get, Update, Delete Product
+app.route("/:id").get(getSingleProduct).put(adminOnly,singleUpload, updateProduct).delete(adminOnly,deleteProduct);
+
+export default app;
